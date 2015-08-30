c9c70261381019afe5017b28e3735fbd50c2585a
Guillem Godoy <ggodoy@lsi.upc.edu>
2015-08-30 12:44:32 +0200

    sonido de los que andan y actualizacion ejecutarcomandos.cc

---
+++ generacomandosonido.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include <cstdio>
+#include <cstdlib>
+#include <time.h>
+#include <unistd.h>
+
+using namespace std;
+
+void morir(string s)
+{
+  cout<<s<<endl;
+  exit(0);
+}
+
+vector<string> leerficherosepararstrings(string nombrefichero)
+{
+  vector<string> v;
+  ifstream ifs;
+  ifs.open(nombrefichero.c_str());
+  if (ifs.is_open()) {
+    string s;
+    while (ifs>>s)
+      v.push_back(s);
+    ifs.close();
+  } else {
+    morir("Error interno al abrir "+nombrefichero);
+  }
+  return v;
+}
+
+string transforma(string s)
+{
+  string r;
+  for (int i=0;i<int(s.size());i++) {
+    if (s[i]!='o')
+      r+=string(2,s[i]);
+    else
+      r+=string(4,s[i]);
+  }
+  return r;
+}
+
+int posicionbarra(string s)
+{
+  for (int i=0;i<int(s.size());i++)
+    if (s[i]=='/')
+      return i+1;
+  return 0;
+}
+
+int main()
+{
+  string persona;
+  cin>>persona;
+  string comando="ls audiodibujosanimados/"+persona+"*.wav >listaficheros.txt";
+  system(comando.c_str());
+  vector<string> v=leerficherosepararstrings("listaficheros.txt");
+  for (int i=0;i<int(v.size());i++) {
+    string nomaux=v[i].substr(posicionbarra(v[i]));
+    cout<<"sonido "+nomaux<<endl<<endl;
+  }
+}
